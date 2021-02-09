d8e395ae163abdcffe6ba45900cffda1f7840a03
Martin <martin.castro1575@gmail.com>
2021-02-09 11:06:35 -0300

    comments update

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 
-# Create your tests here.
+#Creando clases de prueba para probar conexion de paginas
 class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
